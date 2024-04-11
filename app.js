c84a4a966667764f5883dfa0df6ce8e65a1b2c5b
Edwin_K <edumwas911@gmail.com>
2024-04-11 12:14:39 +0300

    separate controllers and routes

--- app.js
+++ app.js
@@ -1,37 +1,31 @@
-const express = require("express");
-const fs = require('fs');
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
-const tours = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`);
-
-app.get('/api/v1/tours', (req,res) =>{
-    res.status(200).json({
-        ststus:'success',
-        results:tours.length,
-        data:{
-            tours
-        }
-    })
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
 });
 
-app.post('/api/v1/tours', (req,res) =>{
-    const newId = tours[tours.length-1].id +1;
-    const newTour = Object.assign({id:newId},req.body)
-    tours.push(newTour)
-    fs.writefile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err => {
-        res.status(201).json({
-            status:'success',
-            data:{
-                tour:newTour
-            }
-        }
-        )
-    })
-})
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
-const port = 3000
-app.listen(port, ()=>{
-    console.log(`App running on port ${port}`)
-})
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
